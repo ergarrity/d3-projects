ee4f30e878b662807f3c91fe8370c1605a52008c
Liz Garrity <ergarrity@gmail.com>
2019-02-10 11:54:54 -0800

    Implemented xScale and yScale

--- bar-chart/bar-chart.js
+++ bar-chart/bar-chart.js
@@ -1,6 +1,11 @@
+
 document.addEventListener("DOMContentLoaded", function(){
     let url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
     let json, dataset, x;
+    
+    const w = 800;
+    const h = 400;
+    const padding = 60;
 
     //XMLHttpRequest to retrieve JSON data from external url
     req = new XMLHttpRequest();
@@ -13,5 +18,16 @@ document.addEventListener("DOMContentLoaded", function(){
         for (item in json.data) {
             dataset.push(json.data[item]);
         }
+
+        const xScale = d3.scaleTime()
+            .domain([
+                d3.min(dataset, d => Date.parse(d[0])), 
+                d3.max(dataset, d => Date.parse(d[0]))
+            ])
+            .range([padding, w - padding]);
+
+        const yScale = d3.scaleLinear()
+            .domain([0, d3.max(dataset, d => d[1])])
+            .range([h - padding, padding]);
     }
 });
