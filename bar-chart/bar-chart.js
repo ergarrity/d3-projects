a5678d6cd05b33da09c65df3f1cadab666a64ac3
Liz Garrity <ergarrity@gmail.com>
2019-02-10 11:19:56 -0800

    Looped over data attribute in JSON response object and pushed each instance to dataset array to isolate data from other attributes (dataset is now an array of arrays, each element representing one quarter's GDP data).

--- bar-chart/bar-chart.js
+++ bar-chart/bar-chart.js
@@ -1,12 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(){
     let url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
-    let json;
+    let json, dataset, x;
 
+    //XMLHttpRequest to retrieve JSON data from external url
     req = new XMLHttpRequest();
     req.open("GET", url, true);
     req.send();
     req.onload = function() {
         json = JSON.parse(req.responseText);
-        console.log(json);
+
+        dataset = [];
+        for (item in json.data) {
+            dataset.push(json.data[item]);
+        }
     }
-})
+});
