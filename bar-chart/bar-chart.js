3b97ffc57571e4689969b3155413e1d88d95da5b
Liz Garrity <ergarrity@gmail.com>
2019-02-13 12:27:32 -0800

    Removed title h1 from bar-chart.html and implemented using d3 in bar-chart.js.

--- bar-chart/bar-chart.js
+++ bar-chart/bar-chart.js
@@ -7,6 +7,11 @@ document.addEventListener("DOMContentLoaded", function(){
     const h = 400;
     const padding = 60;
 
+    const title = d3.select("body")
+                    .append("h1")
+                    .attr("id", "title")
+                    .text("United States GDP")
+
     //XMLHttpRequest to retrieve JSON data from external url
     req = new XMLHttpRequest();
     req.open("GET", url, true);
@@ -29,5 +34,20 @@ document.addEventListener("DOMContentLoaded", function(){
         const yScale = d3.scaleLinear()
             .domain([0, d3.max(dataset, d => d[1])])
             .range([h - padding, padding]);
+
+        const svg = d3.select("body")
+            .append("svg")
+            .attr("width", w)
+            .attr("height", h);
+
+        svg.selectAll("rect")
+            .data(dataset)
+            .enter()
+            .append("rect")
+            .attr("class", "bar")
+            .attr("data-date", d => d[0])
+            .attr("data-gdp", d => d[1])
+            .attr("x", d => d[0])
+
     }
 });
