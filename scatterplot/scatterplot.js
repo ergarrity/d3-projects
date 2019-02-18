a73acc1056894fa5c425941f3755e43b447e94ae
Liz Garrity <ergarrity@gmail.com>
2019-02-17 21:28:39 -0800

    Mapped data to circle elements and appended to svg

--- scatterplot/scatterplot.js
+++ scatterplot/scatterplot.js
@@ -37,5 +37,47 @@ document.addEventListener("DOMContentLoaded", function(){
                 "doping": Doping
             });
         }
+
+        // Define SVG element that will contain visualization
+        const svg = d3.select("body")
+            .append("svg")
+            .attr("width", w)
+            .attr("height", h)
+            .attr("class", "scatterplot");
+
+        // Define scales
+        const xScale = d3.scaleLinear()
+            .domain([
+                d3.min(dataset, d => d.year),
+                d3.max(dataset, d => d.year)
+            ])
+            .range([padding, w - padding]);
+
+        const yScale = d3.scaleLinear()
+            .domain([0, d3.max(dataset, d => d.time)])
+            .range([h - padding, padding]);
+
+        // Define axes according to scales
+        const xAxis = d3.axisBottom(xScale).tickFormat(d3.format("d"));
+        const yAxis = d3.axisLeft(yScale).tickFormat(d3.timeFormat("%M:%S"));
+
+        // Append x axis
+        svg.append("g")
+            .attr("id", "x-axis")
+            .attr("transform", "translate(0, " + (h - padding) + ")")
+            .call(xAxis);
+
+        // Append y axis
+        svg.append("g")
+            .attr("id", "y-axis")
+            .attr("transform", "translate(" + padding + ", 0)")
+            .call(yAxis);
+
+        // Map data to circles
+        svg.selectAll("circle")
+            .data(dataset)
+            .enter()
+            .append("circle")
+            .attr("class", "circle")
     } 
 });
